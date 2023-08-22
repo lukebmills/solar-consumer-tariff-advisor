1319a3681bf1e0a53b894f4e6ce512eb55d0013b
lukebmills <luke.mills@hey.com>
2023-08-22 11:48:04 +0200

    - export tariffs as a data frame - interpolate to match length of time array, t

--- tariff_extractor.py
+++ tariff_extractor.py
@@ -1,15 +1,15 @@
 import openpyxl
+import pandas as pd
+import numpy as np
 
 class EnergyTariff:
     def __init__(self, name="Default Tariff", values=None):
         self.name = name
         self.values = values if values is not None else []
 
-    # ... Other methods here ...
-
     @classmethod
-    def read_arrays_from_excel(cls, filename):
-        tariff_instances = []
+    def read_arrays_from_excel(cls, filename, t):
+        tariff_dataframes = []
 
         try:
             wb = openpyxl.load_workbook(filename)
@@ -18,11 +18,16 @@ class EnergyTariff:
                 for col in sheet.iter_cols(min_col=2, values_only=True):
                     name = col[0]
                     values = col[1:]
+                    values = np.interp(np.linspace(0, len(values) - 1, len(t)), np.arange(len(values)), values)
 
-                    tariff_instance = cls(name, values)
-                    tariff_instances.append(tariff_instance)
+                    df = pd.DataFrame({'Value': values})
+                    df.index.name = 'Hour'
+                    df.columns.name = name
+                    tariff_dataframes.append(df)
+
 
         except Exception as e:
             print("Error reading Excel file:", e)
 
-        return tariff_instances
+
+        return tariff_dataframes

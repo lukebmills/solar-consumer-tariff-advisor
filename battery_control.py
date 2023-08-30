e409202d89646b9e3e2f5b9425cf5cb581cbbd32
lukebmills <luke.mills@hey.com>
2023-08-30 10:37:05 +0100

    - take max charge / discharge as an input from main

--- battery_control.py
+++ battery_control.py
@@ -5,7 +5,7 @@ class BatteryControl:
         pass
     
     @classmethod
-    def find_power_and_SOC(cls, s, h, min_SOC, max_SOC, import_tariff, export_tariff, readings_per_hour):
+    def find_power_and_SOC(cls, s, h, min_SOC, max_SOC, import_tariff, export_tariff, readings_per_hour, max_charge, max_discharge):
         average_import_tariff = import_tariff.mean()
         average_export_tariff = export_tariff.mean()
         reading_intervals = 60/readings_per_hour
@@ -13,7 +13,7 @@ class BatteryControl:
         b = np.zeros_like(s,dtype = float)
         SOC = np.zeros_like(s, dtype=float)
 
-        # if there is a flat import / export tariff pair, employ a simple self-use battery control mechanism
+        # if there is a flat import / export tariff pair, employ a simple self-consumption battery control mechanism
         if np.std(import_tariff) + np.std(export_tariff) == 0:
         
             for i in range (1,len(s)):
@@ -35,14 +35,14 @@ class BatteryControl:
 
             for i in range (1,len(s)):
 
-                # during off peak periods
-                if import_tariff[i]<average_import_tariff and export_tariff[i]>average_export_tariff:
-                    # Charge at 3 kW from the grid
-                    b[i] = 3 + s[i] - h[i]
+                # during off-peak periods
+                if import_tariff[i]<average_import_tariff and export_tariff[i]<average_export_tariff:
+                    # Charge from grid, in kW
+                    b[i] = max_charge + s[i] - h[i]
                 # during peak periods
-                elif import_tariff[i]>average_import_tariff and export_tariff[i]<average_export_tariff:
-                    # Discharge at 3kW to grid
-                    b[i] = -3 + s[i] - h[i]
+                elif import_tariff[i]>average_import_tariff and export_tariff[i]>average_export_tariff:
+                    # Discharge to grid, in kW
+                    b[i] = -max_discharge
                 else:
                     b[i] = s[i] - h[i]
                 # controls to ensure the battery SOC doesn't exceed the limits
@@ -55,7 +55,7 @@ class BatteryControl:
                 # Calculate the state of charge (SOC) based on the cumulative battery discharge rate
                 SOC[i] = SOC[i - 1] + b[i] * reading_intervals / 60
         
-        # determine grid load
+        # determine the resulting grid load
         g = -h - b + s
         return b, SOC, g
     

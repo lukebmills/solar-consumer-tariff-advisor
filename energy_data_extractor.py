7d7a54764987643b8000058acf6e8f01066c1af0
lukebmills <luke.mills@hey.com>
2023-08-30 10:36:59 +0100

    - fix issue with reading SolarEdge inverter power readings

--- energy_data_extractor.py
+++ energy_data_extractor.py
@@ -10,9 +10,6 @@ class EnergyData:
 
     @classmethod
     def extract_data(cls,inverter_readings_path,t):
-        s_values = []
-        h_values = []
-        average_starting_SOC = 0.00
 
         # find folders in current folder
         folders_list = [folder for folder in os.listdir(inverter_readings_path) if os.path.isdir(os.path.join(inverter_readings_path, folder))]
@@ -24,9 +21,9 @@ class EnergyData:
         for i in range(0,len(folders_list)):
             inverter_data_select_input = inverter_data_select_input + str(i+1) + " = " + folders_list[i] + ", "
 
-        # ask which inverter data set to analyse
+        # ask which inverter data set to use
         inverter_data_select = int(input(inverter_data_select_input))-1
-        bill_directory = inverter_readings_path+"/"+folders_list[inverter_data_select]+'/*.xl*'
+        bill_directory = inverter_readings_path+"/"+folders_list[inverter_data_select]
 
         # determine the inverter type based on the folder name. If not recognised then ask
         if inverter_names[inverter_data_select] == 'Solis':
@@ -39,9 +36,9 @@ class EnergyData:
             inverter_type_input = int(input("Which inverter is this data from: 1 = GivEnergy, 2 = Solis, 3 = SolarEdge"))-1
 
         # Get a list of all Excel files in the folder
-        excel_files = glob.glob(bill_directory)
+        excel_files = glob.glob(bill_directory+'/*.xl*')
 
-        # Define structure of different data sets
+        # Define structure of each data set
         data_headers = [
                 ['Solar Generation', 'Total Inverter Power(W)', 'Solar Production (W)'],
                 ['Load Power', 'Total Consume Energy Power(W)', 'Consumption (W)'],
@@ -54,9 +51,8 @@ class EnergyData:
         data_start_rows = [1, 8, 1] # define starting rows for each data set
         target_sheet_names = [0, 0, 0] # define sheet of each excel file that data will be pulled from
         inverter_battery_sign = [-1,1,1] # define battery power data as positive or negative based on inverter type
-
-        data_start_row = data_start_rows[inverter_type_input]
-        target_sheet_name = target_sheet_names[inverter_type_input]
+        data_start_row = data_start_rows[inverter_type_input] # assign the starting row
+        target_sheet_name = target_sheet_names[inverter_type_input] # assign the target sheet name
 
         # create an empty list to store the data frames
         dfs = []
@@ -69,9 +65,21 @@ class EnergyData:
         df = pd.concat(dfs, ignore_index=True)
 
         # Assign headers and date format based on inverter type input
-        chosen_inverter_data_headers = []
+        chosen_inverter_data_headers = [] # create an empty array for the headers
+        inner_list_counter = 0
         for inner_list in data_headers:
-            chosen_inverter_data_headers.append(inner_list[inverter_type_input])
+            inner_list_counter = inner_list_counter+1
+
+            # append the headers for each data point except for the SolarEdge's Grid Power reading
+            if inner_list_counter < 5 or inverter_type_input != 2:
+                chosen_inverter_data_headers.append(inner_list[inverter_type_input])
+
+            # Add two separate data headers for SolarEdge's grid readings
+            if inner_list_counter == 5 and inverter_type_input ==2:
+                chosen_inverter_data_headers.append('Import (W)')
+                chosen_inverter_data_headers.append('Export (W)')
+
+        # set the appropriate date time format for the inverter
         date_time_format = date_time_formats[inverter_type_input]
 
         # Convert the 'Time' column to pandas datetime and average to nearest time interval
@@ -82,15 +90,20 @@ class EnergyData:
 
         # Extract and process data values
         data_values = [df.groupby('Time')[header].mean() for header in chosen_inverter_data_headers]
-        # divide battery SOC % values by 100 to go from % to decimals and other figures by 1000 to go from W to kW
+
+        # divide battery SOC % values by 100 and multiply by battery capacity to go from % to kWh and other figures by 1000 to go from W to kW
         data_values = [values / 100 if i == 3 else values / 1000 for i, values in enumerate(data_values)]
+
+        # for SolarEdge inverters, sum the 'import' and 'export' columns
+        if inverter_type_input == 2:
+            data_values[4] = data_values[4]+data_values[5]
 
         # find the average SOC at the start of each day
         average_starting_SOC = data_values[3][0]
 
-        # Interpolate data to match the time data
+        # Interpolate data to align with time intervals
         t = np.arange(len(data_values[0]))
         interpolated_values = [np.interp(np.linspace(0, len(values) - 1, len(t)), np.arange(len(values)), values)
                                for values in data_values]
 
-        return interpolated_values[0], interpolated_values[1], interpolated_values[2], interpolated_values[3], interpolated_values[4], average_starting_SOC
+        return interpolated_values[0], interpolated_values[1], interpolated_values[2]*inverter_battery_sign[inverter_type_input], interpolated_values[3], interpolated_values[4], average_starting_SOC, bill_directory
